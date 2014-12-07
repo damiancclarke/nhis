47c61d6dff8c01f15083a77f0c63ba86497856e1
damiancclarke <damian.c.clarke@gmail.com>
2014-12-07 13:01:34 -0300

    Script description

---
+++ NHISprocess.py
@@ -0,0 +1,17 @@
+#   NHISprocess.py v0.00         damiancclarke             yyyy-mm-dd:2014-12-08
+#---|----1----|----2----|----3----|----4----|----5----|----6----|----7----|----8
+#
+#This script downloads and processes all NHIS (National Health Interview Survey)
+#data, resulting in a series of Stata dta files for each year from 1997-2013. T-
+#his is the series of the survey after changes were made in 1996. To run the sc-
+#ript, python 2.x is required.  The syntax is the following
+# 
+#  python NHISprocess.py DLDIR OUTDIR
+#
+#where DLDIR is the directory where web based downloads are sent on the computer
+#and OUTDIR is the directory where processed files should be saved after the sc-
+#ript terminates.
+#
+#This file is made available under the GNU Public License version 3. For questio-
+#ns and comments:
+#                     mailto:damian.clarke@economics.ox.ac.uk
